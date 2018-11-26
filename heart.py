bcb2a7bd60a083a01bcc1e6672874bd071d1f25c
Kurniandha S.Y <ykurniandha@gmail.com>
2018-11-26 19:20:45 +0700

    Update Flask

--- heart.py
+++ heart.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,18 +11,19 @@ def index():
 
 @app.route('/b', methods = ['POST'])
 def run():
-	result = request.form
+    result = request.form['Mathematics']
+
     # Load Model
     with open('model.sav', 'rb') as file :
         model = pickle.load(file)
 
-    # Load Datatest
-    df = pd.read_csv('tubes2_HeartDisease_test.csv')
+    # Create dataform
+    data = np.array([[54,1,4,-0.044575,0.431818,0.0,-1.784928,1.0,-0.324500,1,0,0,0,1,0]])
 
     # Predict
-    predict = model.predict(df)
+    predict = model.predict(data)
 
-    return render_template('b.php', key = result)
+    return render_template('b.php', result = predict[0])
 
 if __name__ == '__main__' :
     app.run(debug = True)
